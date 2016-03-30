b25adc20663b688e656bd897506760593f1fd046
Erik Fredner <erikfredner@gmail.com>
2016-03-30 15:05:42 -0700

    Resetting the Work model.

--- data/models.py
+++ data/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 class Anthology(models.Model):
@@ -9,29 +10,25 @@ class Anthology(models.Model):
     def __str__(self):
         return self.title
 
-class Category(models.Model):
-
-    anthology = models.ForeignKey('Anthology', null = True)
-    parent = models.ForeignKey('Category', null = True, blank = True)
-    label = models.CharField(max_length = 200)
-
-    def __str__(self):
-        return self.label
-
-    @property
-    def parent_anthology(self):
-        if self.anthology:
-            return self.anthology
-        else:
-            return self.parent.anthology
-
 class Author(models.Model):
 
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=20, null = True, blank = True)
+    last_name = models.CharField(max_length=200, null = True, blank = True)
     birth_year = models.IntegerField(null = True)
     death_year = models.IntegerField(null = True)
     circa = models.BooleanField(default = False)
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
+
+class Work(models.Model):
+
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author')
+    parent = models.ForeignKey('Work', null = True, blank = True)
+
+    def __str__(self):
+        return self.title + ' by ' + str(self.author)
+
+    def clean(self):
+        pass

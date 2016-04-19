12cc1f0111e2cec8372e07f09e529bb1e180adaa
Erik Fredner <erikfredner@gmail.com>
2016-04-19 11:45:04 -0700

    Add select widgets to author and parent on Work.

--- data/admin.py
+++ data/admin.py
@@ -2,7 +2,7 @@
 
 from django.contrib import admin
 from django import forms
-from django_select2.forms import Select2Widget
+from django_select2.forms import Select2Widget, Select2MultipleWidget
 
 from .models import Author, Work, Anthology
 
@@ -11,8 +11,12 @@ class WorkForm(forms.ModelForm):
 
     class Meta:
         model = Work
-        widgets = dict(anthologies=Select2Widget)
         fields = '__all__'
+        widgets = dict(
+            anthologies=Select2MultipleWidget,
+            author=Select2Widget,
+            parent=Select2Widget,
+        )
 
 
 

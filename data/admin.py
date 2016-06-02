289042d597edd906f653aa288cef2be698505dcb
Erik Fredner <erikfredner@gmail.com>
2016-06-02 12:08:19 -0700

    On 'save and add another,' prepopulate the work form with author and anthologies.

--- data/admin.py
+++ data/admin.py
@@ -1,8 +1,12 @@
 
 
+from urllib.parse import urlencode
+
 from django.contrib import admin
 from django import forms
 from django_select2.forms import Select2Widget, Select2MultipleWidget
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 from .models import Author, Work, Anthology
 
@@ -20,8 +24,35 @@ class WorkForm(forms.ModelForm):
 
 
 class WorkAdmin(admin.ModelAdmin):
+
     form = WorkForm
     search_fields = ['title', 'author__name']
+
+    def response_add(self, request, obj, post_url_continue=None):
+
+        """
+        On "add another," pre-populate author and anthologies.
+        """
+
+        if '_addanother' in request.POST:
+
+            url = reverse('admin:data_work_add')
+
+            author_id = request.POST['author']
+            anthology_ids = request.POST.getlist('anthologies')
+
+            params = {
+                'author': author_id,
+                'anthologies': ','.join(anthology_ids),
+            }
+
+            url += '?'+urlencode(params)
+
+            return HttpResponseRedirect(url)
+
+        else:
+            return HttpResponseRedirect(reverse("admin:data_work_changelist"))
+
 
 
 class AuthorAdmin(admin.ModelAdmin):

aa9cf21ad0d4543b4c1682eac76245591ca37e06
David McClure <davidwilliammcclure@gmail.com>
2016-06-21 11:50:27 -0700

    Point to settings.production in the wsgi app.

--- norton/wsgi.py
+++ norton/wsgi.py
@@ -12,6 +12,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "norton.settings")
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'norton.settings.production',
+)
 
 application = get_wsgi_application()

338207f069bb1d1938eb4b10d9ac10dfba30e733
David McClure <davidwilliammcclure@gmail.com>
2016-10-21 15:18:01 -0700

    Use select2 on the country field, set COUNTRIES_FIRST.

--- data/admin/author_admin.py
+++ data/admin/author_admin.py
@@ -2,6 +2,11 @@
 
 from django.contrib import admin
 
+from .author_form import AuthorForm
+
 
 class AuthorAdmin(admin.ModelAdmin):
+
+    form = AuthorForm
+
     search_fields = ['name']
--- norton/settings/base.py
+++ norton/settings/base.py
@@ -125,3 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Countries
+# https://github.com/SmileyChris/django-countries
+
+COUNTRIES_FIRST = [
+    'US',
+    'GB',
+    'IE',
+]

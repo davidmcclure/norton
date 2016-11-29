cd1ddcd5b03b4074fb39620a343422d066021b6a
David McClure <davidwilliammcclure@gmail.com>
2016-11-29 11:58:52 -0800

    Jupyter harness.

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+
+    name='norton',
+    version='0.1.0',
+    author='David McClure',
+    author_email='dclure@stanford.edu',
+    packages=find_packages(),
+
+)
